d142aa17d009e51b373b4d9d0a90b06a1032b479
Lukas Reimer <lukas.reimer@burohappold.com>
2019-05-21 20:18:00 +0200

    Implemented Level output from linked and active model

--- RevitPythonScripts/RoomSpaceCopy.py
+++ RevitPythonScripts/RoomSpaceCopy.py
@@ -41,10 +41,58 @@ for room in rooms:
 print("Total = {0} rooms".format(len(rooms)))
 print("")
 
-# Create Spaces for all rooms in the selected link
-# Transfer data from the linked rooms to the Spaces
+# Get linked Levels:
+linked_levels = db.FilteredElementCollector(link)\
+                  .OfCategory(db.BuiltInCategory.OST_Levels)\
+                  .WhereElementIsNotElementType()\
+                  .ToElements()
 
-print("Done.")
+print("Linked Levels:")
+for level in linked_levels:
+    print(level, level.Name, level.Elevation)
+print("Total = {0} linked levels".format(len(linked_levels)))
+print("")
+
+
+# Get Levels:
+levels = db.FilteredElementCollector(doc)\
+           .OfCategory(db.BuiltInCategory.OST_Levels)\
+           .WhereElementIsNotElementType()\
+           .ToElements()
+
+print("Levels:")
+for level in levels:
+    print(level, level.Name, level.Elevation, level.IsMonitoringLinkElement())
+print("Total = {0} levels".format(len(levels)))
+print("")
+
+# TODO: Create Mapping of Levels and Linked Levels by minimal distance
+
+transaction = db.Transaction(doc)
+transaction.Start("RoomSpaceCopy.py")
+try:
+    pass
+    # Create Spaces for all rooms in the selected link
+    for room in rooms:
+        room_level = room.Level
+        space_level = room_level  # TODO: find correct level from mapping
+        location_point = room.Location.Point
+        insert_point = db.UV(location_point.X, location_point.Y)
+        # print(level, location, point)
+        # print("creating space: ")
+        # created_space = doc.Create.NewSpace(space_level, insert_point)
+        # print("created space: {0}".format(created_space))
+    # Transfer data from the linked rooms to the Spaces
+
+except Exception as ex:
+    print(ex)
+    transaction.RollBack()
+else:
+    transaction.Commit()
+    print("Done.")
+
+
+
 
 #revit python shell has a console, access it like so
 #__window__.Hide()
