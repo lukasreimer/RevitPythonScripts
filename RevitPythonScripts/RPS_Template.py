4509e693da0e8e18371a4424152e53e99899a377
Lukas Reimer <lukas.reimer@burohappold.com>
2019-05-31 15:57:23 +0200

    Update RPS_Template.py

--- RevitPythonScripts/RPS_Template.py
+++ RevitPythonScripts/RPS_Template.py
@@ -1,22 +1,40 @@
-#Typical python revit shell
-#===========================
+"""Module Docstring.
 
-
-#import routine
-#------------------
+This is a concise description of the script.
+"""
+# standard library imports
+import time
 import clr
+# third party imports
 clr.AddReference('RevitAPI') 
 clr.AddReference('RevitAPIUI') 
-from Autodesk.Revit.DB import * 
+import Autodesk.Revit.DB as db
+# local imports
+# ...
 
-#important vars, revit python shell version
-#-------------------------------
-app = __revit__.Application
-doc = __revit__.ActiveUIDocument.Document
-uidoc = __revit__.ActiveUIDocument
-view = doc.ActiveView 
+# Main Script
+def main(app, doc, uidoc, view):
+    """Function Docstring.
 
-#revit python shell has a console, access it like so
-#---------------------------------------------------
-__window__.Hide()
-__window__.Close()
+    This is a description of the main function containing the business logic.
+    """
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    start = time.clock()
+
+    # important revit python shell variables
+    app = __revit__.Application
+    doc = __revit__.ActiveUIDocument.Document
+    uidoc = __revit__.ActiveUIDocument
+    view = doc.ActiveView
+
+    main(app, doc, uidoc, view)
+
+    runtime = time.clock() - start
+    print("Runtime = {0} seconds".format(runtime))
+
+    # revit python shell console management
+    # __window__.Hide()
+    # __window__.Close()
