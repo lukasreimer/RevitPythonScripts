13f39a68fa5df51fd8d39d2afcffc29f9d5d9ea6
Lukas Reimer <lukas.reimer@burohappold.com>
2019-06-04 18:57:01 +0200

    Implemented space creation with RoomSpaceCopy script

--- RevitPythonScripts/RoomSpaceCopy.py
+++ RevitPythonScripts/RoomSpaceCopy.py
@@ -1,6 +1,7 @@
 """Create Spaces from linked Rooms."""
 
 import time
+import sys
 import clr
 clr.AddReference('RevitAPI')
 clr.AddReference('RevitAPIUI')
@@ -16,7 +17,7 @@ def main():
     uidoc = __revit__.ActiveUIDocument
     view = doc.ActiveView
 
-    print("Running RoomSpaceCopy script for copying linked Rooms to Spaces...")
+    print("Running RoomSpaceCopy script copying linked Rooms to Spaces...")
 
     # Get all links in the revit model
     linked_docs = []
@@ -24,12 +25,13 @@ def main():
         if document.IsLinked:
             linked_docs.append(document)
 
-    print("Linked Documents:")
-    print(linked_docs)
-    for item in linked_docs:
-        print(item)
-        print("{0}: {1}".format(item.Title, item.PathName))
+    # print("Linked Documents:")
+    # print(linked_docs)
+    # for item in linked_docs:
+    #     print(item)
+    #     print("{0}: {1}".format(item.Title, item.PathName))
 
+    # TODO: implement user selection of the document
     # Select the link to copy rooms from
     link = linked_docs[0]
     print("Selected Link: {}".format(link.Title))
@@ -39,21 +41,21 @@ def main():
             .OfCategory(db.BuiltInCategory.OST_Rooms)\
             .ToElements()
 
-    print("Linked Rooms:")
-    for room in rooms:
-        print(room)
-    print("Total = {0} rooms".format(len(rooms)))
+    # print("Linked Rooms:")
+    # for room in rooms:
+    #     print(room)
+    print("Found {0} rooms in the linked document".format(len(rooms)))
 
-    # Get linked Levels:
-    linked_levels = db.FilteredElementCollector(link)\
-                    .OfCategory(db.BuiltInCategory.OST_Levels)\
-                    .WhereElementIsNotElementType()\
-                    .ToElements()
+    # # Get linked Levels:
+    # linked_levels = db.FilteredElementCollector(link)\
+    #                 .OfCategory(db.BuiltInCategory.OST_Levels)\
+    #                 .WhereElementIsNotElementType()\
+    #                 .ToElements()
 
-    print("Linked Levels:")
-    for level in linked_levels:
-        print(level, level.Name, level.Elevation)
-    print("Total = {0} linked levels".format(len(linked_levels)))
+    # print("Linked Levels:")
+    # for level in linked_levels:
+    #     print(level, level.Name, level.Elevation)
+    # print("Total = {0} linked levels".format(len(linked_levels)))
 
     # Get Levels:
     levels = db.FilteredElementCollector(doc)\
@@ -61,26 +63,25 @@ def main():
             .WhereElementIsNotElementType()\
             .ToElements()
 
-    print("Levels:")
-    for level in levels:
-        print(level, level.Name, level.Elevation, level.IsMonitoringLinkElement())
-    print("Total = {0} levels".format(len(levels)))
+    # print("Levels:")
+    # for level in levels:
+    #     print(level, level.Name, level.Elevation)
+    print("Found {0} levels in the model.".format(len(levels)))
 
-    # TODO: Create Mapping of Levels and Linked Levels by minimal distance
+    # Create Spaces for all placed Rooms in the selected link
+    print("Creating spaces for placed rooms in the selected link...")
     transaction = db.Transaction(doc)
     transaction.Start("RoomSpaceCopy.py")
     try:
-        pass
-        # Create Spaces for all rooms in the selected link
+        created_spaces = []
         for room in rooms:
-            room_level = room.Level
-            space_level = room_level  # TODO: find correct level from mapping
-            location_point = room.Location.Point
-            insert_point = db.UV(location_point.X, location_point.Y)
-            # print(level, location, point)
-            # print("creating space: ")
-            # created_space = doc.Create.NewSpace(space_level, insert_point)
-            # print("created space: {0}".format(created_space))
+            space_level = find_closest_level(levels, room.Level.Elevation)
+            if room.Location:  # room is actually placed
+                location_point = room.Location.Point
+                insert_point = db.UV(location_point.X, location_point.Y)
+                created_space = doc.Create.NewSpace(space_level, insert_point)
+                created_spaces.append(created_space)
+        print("Created {0} spaces.".format(len(created_spaces)))
         # Transfer data from the linked rooms to the Spaces
 
     except Exception as ex:
@@ -90,9 +91,20 @@ def main():
         transaction.Commit()
         print("Done.")
 
+def find_closest_level(levels, elevation):
+    """Find the level closest to the given elevation. """
+    closest = None
+    difference = float("inf")
+    for level in levels:
+        level_difference = abs(level.Elevation - elevation)
+        if level_difference < difference:
+            closest = level
+            difference = level_difference
+    return closest
+
 
 if __name__ == "__main__":
-     start = time.clock()
+    start = time.clock()
     main()
     runtime = time.clock() - start
     print("Runtime = {0} seconds".format(runtime))
