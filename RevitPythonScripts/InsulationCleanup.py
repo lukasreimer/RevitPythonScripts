2cb9a0331eb081da39aeea74fa002a1778d8a196
Lukas Reimer <lukas.reimer@burohappold.com>
2019-06-11 16:34:29 +0200

    Refactoring of insulation cöeanup script

--- RevitPythonScripts/InsulationCleanup.py
+++ RevitPythonScripts/InsulationCleanup.py
@@ -2,12 +2,12 @@
 
 import time
 import clr
-clr.AddReference('RevitAPI')
-clr.AddReference('RevitAPIUI')
+
+clr.AddReference("RevitAPI")
+clr.AddReference("RevitAPIUI")
 import Autodesk.Revit.DB as db
 import Autodesk.Revit.UI as ui
 import collections
-import sys
 
 
 # TODO: Cleanup and console/dialog data output
@@ -23,32 +23,28 @@ def main():
     duct_insulation_category = db.BuiltInCategory.OST_DuctInsulations
 
     # Main Script
-    print("Running InsulationCleanup script...")
+    print("Running InsulationCleanup.py script...")
     # Inspect
     # select all pipe/duct insulation elements
-    pipe_insulation_elements = query_all_elements(document=doc, category=pipe_insulation_category)
-    duct_insulation_elements = query_all_elements(document=doc, category=duct_insulation_category)
+    pipe_insulation_elements = query_all_elements(
+        document=doc, category=pipe_insulation_category
+    )
+    duct_insulation_elements = query_all_elements(
+        document=doc, category=duct_insulation_category
+    )
 
     print_summary(pipe_insulation_elements, "Pipe Insulation Summary:")
     print_summary(duct_insulation_elements, "Duct Insulation Summary:")
     # print_all(pipe_insulation_elements, "Pipe Insulation Elements:")
     # print_all(duct_insulation_elements, "Duct Insulation Elements:")
 
-    sys.exit()
-
     # Find all unhosted and rogue insulation elements
-    ElementHostPair = collections.namedtuple("ElementHostPair", ["element", "host"])
-    unhosted_elements = []
-    rogue_elements = []
-    for element in insulation_elements:
-        host_id = element.HostElementId
-        host_element = doc.GetElement(host_id)
-        if host_element is None:
-            unhosted_elements.append(element)
-        else:
-            if element.WorksetId != host_element.WorksetId:
-                rogue_elements.append(ElementHostPair(element, host_element))
-        # print("{id}: {element}".format(id=host_id, element=host_element))
+    rogue_pipe, unhosted_pipe = find_rogue_elements(
+        document=doc, insulation_elements=pipe_insulation_elements
+    )
+    rogue_duct, unhosted_duct = find_rogue_elements(
+        document=doc, insulation_elements=duct_insulation_elements
+    )
 
     # show a report of all unhosted/rogue elements
     # print("{} Unhosted Elements:".format(len(unhosted_elements)))
@@ -70,41 +66,69 @@ def main():
     transaction.Start("InsulationCleanup.py")
     try:
         # Delete all unhosted elements
-        for element in unhosted_elements:
+        for element in unhosted_pipe:
             doc.Delete(element.Id)
         # Move all rogue elements to correct workset
-        for pair in rogue_elements:
-            # https://thebuildingcoder.typepad.com/blog/2013/01/change-element-workset.html
-            element_workset_id = pair.element.WorksetId.IntegerValue
-            host_workset_id = pair.host.WorksetId.IntegerValue
-            print("{elem_id}, {host_id}".format(elem_id=element_workset_id, host_id=host_workset_id))
-
-            # get the host workset parameter for setting its value
-            host_workset_parameter = pair.host.get_Parameter(db.BuiltInParameter.ELEM_PARTITION_PARAM)
-            # move host to insulation WorksetId
-            host_workset_parameter.Set(element_workset_id)
-            # move host to previous host workset (and carry over the insulation)
-            host_workset_parameter.Set(host_workset_id)
-
+        for pair in rogue_pipe:
+            cleanup_insulation(pair)
         # raise RuntimeError("Just testing rolling back the transaction.")
     except Exception as ex:
         ui.TaskDialog.Show("Failed", "Exception:\n{}".format(ex))
         transaction.RollBack()
     else:
-        ui.TaskDialog.Show("Done", "Deleted {num_unhosted} unhosted insulation elements.\nMoved {num_rogue} rogue insulation elements".format(num_unhosted=len(unhosted_elements), num_rogue=len(rogue_elements)))
+        ui.TaskDialog.Show(
+            "Done",
+            "Deleted {num_unhosted} unhosted insulation elements.\nMoved {num_rogue} rogue insulation elements".format(
+                num_unhosted=len(unhosted_elements), num_rogue=len(rogue_elements)
+            ),
+        )
         transaction.Commit()
 
 
 # Functions:
+ElementHostPair = collections.namedtuple("ElementHostPair", ["element", "host"])
+
+
+def cleanup_insulation(pair):
+    """Cleanup rogue insulation elements."""
+    # https://thebuildingcoder.typepad.com/blog/2013/01/change-element-workset.html
+    element_workset_id = pair.element.WorksetId.IntegerValue
+    host_workset_id = pair.host.WorksetId.IntegerValue
+    # print("{elem_id}, {host_id}".format(elem_id=element_workset_id, host_id=host_workset_id))
+    # get the host workset parameter for setting its value
+    host_workset_parameter = pair.host.get_Parameter(
+        db.BuiltInParameter.ELEM_PARTITION_PARAM
+    )
+    # move host to insulation WorksetId
+    host_workset_parameter.Set(element_workset_id)
+    # move host to previous host workset (and carry over the insulation)
+    host_workset_parameter.Set(host_workset_id)
+
+
+def find_rogue_elements(document, insulation_elements):
+    """Find all unhosted or rogue insulation elements."""
+    unhosted_elements = []
+    rogue_elements = []
+    for element in insulation_elements:
+        host_id = element.HostElementId
+        host_element = document.GetElement(host_id)
+        if host_element is None:
+            unhosted_elements.append(element)
+        else:
+            if element.WorksetId != host_element.WorksetId:
+                rogue_elements.append(ElementHostPair(element, host_element))
+        # print("{id}: {element}".format(id=host_id, element=host_element))
+    return rogue_elements, unhosted_elements
+
+
 def query_all_elements(document, category):
     """Return all elements of a category from a document."""
     filter = db.ElementCategoryFilter(category)
     collector = db.FilteredElementCollector(document)
-    elements = collector.WherePasses(filter)\
-                        .WhereElementIsNotElementType()\
-                        .ToElements()
+    elements = collector.WherePasses(filter).WhereElementIsNotElementType().ToElements()
     # pythonic_elements = [element for element in elements]
     return elements
+
 
 def print_summary(element_list, caption=None):
     """Print a summary of the given list."""
