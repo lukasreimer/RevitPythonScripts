b6c64d7a5fe1a512868968b5455dad0574e8fdd9
Lukas Reimer <lukas.reimer@icloud.com>
2019-06-23 12:36:47 +0200

    Refactored InsulationCleanup to have shorter more readable lines

--- RevitPythonScripts/InsulationCleanup.py
+++ RevitPythonScripts/InsulationCleanup.py
@@ -1,12 +1,14 @@
 """Cleanup unhosted and rogue pipe and duct insulation."""
 
-import time
+import collections
+import os
 import clr
 
 clr.AddReference("RevitAPI")
 import Autodesk.Revit.DB as db
-import collections
-import os
+
+clr.AddRefrence("RevitAPIUI")
+import Autodesk.Revit.UI as ui
 
 
 def main():
@@ -14,8 +16,8 @@ def main():
 
     # Setup
     doc = __revit__.ActiveUIDocument.Document
-    pipe_insulation_category = db.BuiltInCategory.OST_PipeInsulations
-    duct_insulation_category = db.BuiltInCategory.OST_DuctInsulations
+    pipe_ins_cat = db.BuiltInCategory.OST_PipeInsulations
+    duct_ins_cat = db.BuiltInCategory.OST_DuctInsulations
     clean_pipes = False
     clean_ducts = False
     report_file_name = "InsulationCleanup.txt"
@@ -24,20 +26,16 @@ def main():
     print("Running InsulationCleanup.py script...")
 
     # STEP 1: Inspect Model
-    pipe_insulation_elements = query_all_elements(
-        document=doc, category=pipe_insulation_category)
-    duct_insulation_elements = query_all_elements(
-        document=doc, category=duct_insulation_category)
+    pipe_ins_elems = query_all_elements(doc=doc, cat=pipe_ins_cat)
+    duct_ins_elems = query_all_elements(doc=doc, cat=duct_ins_cat)
 
-    print_summary(pipe_insulation_elements, "Pipe Insulation Elements:")
+    print_summary(pipe_ins_elems, "Pipe Insulation Elements:")
     # print_all(pipe_insulation_elements, indent=2)
-    print_summary(duct_insulation_elements, "Duct Insulation Elements:")
+    print_summary(duct_ins_elems, "Duct Insulation Elements:")
     # print_all(duct_insulation_elements, indent=2)
 
-    rogue_pipe, unhosted_pipe = find_rogue_elements(
-        document=doc, insulation_elements=pipe_insulation_elements)
-    rogue_duct, unhosted_duct = find_rogue_elements(
-        document=doc, insulation_elements=duct_insulation_elements)
+    rogue_pipe, unhosted_pipe = find_rogue_elements(doc=doc, elems=pipe_ins_elems)
+    rogue_duct, unhosted_duct = find_rogue_elements(doc=doc, elems=duct_ins_elems)
 
     print_summary(unhosted_pipe, "Unhosted Pipe Insulation Summary:")
     # print_all(unhosted_pipe, indent=2)
@@ -49,24 +47,24 @@ def main():
     #print_all(rogue_duct, indent=2)
 
     # STEP 2: Receive User Input
-    print("[0] Report, [1] Clean Pipe, [2] Clean Duct, [3] Clean Both")
-    answer = raw_input("?> ").strip()
-    if answer == "0":
-        full_path = os.path.join(os.getcwd(), report_file_name)
-        write_report(file_path=full_path,
-                     unhosted_pipe=unhosted_pipe,
-                     rogue_pipe=rogue_pipe,
-                     unhosted_duct=unhosted_duct,
-                     rogue_duct=rogue_duct)
-    elif answer == "1":
-        clean_pipes = True
-    elif answer == "2":
-        clean_ducts = True
-    elif answer == "3":
-        clean_pipes = True
-        clean_ducts = True
-    else:
-        print("Nothing to do...")
+    # print("[0] Report, [1] Clean Pipe, [2] Clean Duct, [3] Clean Both")
+    # answer = raw_input("?> ").strip()
+    # if answer == "0":
+    #     full_path = os.path.join(os.getcwd(), report_file_name)
+    #     write_report(file_path=full_path,
+    #                  unhosted_pipe=unhosted_pipe,
+    #                  rogue_pipe=rogue_pipe,
+    #                  unhosted_duct=unhosted_duct,
+    #                  rogue_duct=rogue_duct)
+    # elif answer == "1":
+    #     clean_pipes = True
+    # elif answer == "2":
+    #     clean_ducts = True
+    # elif answer == "3":
+    #     clean_pipes = True
+    #     clean_ducts = True
+    # else:
+    #     print("Nothing to do...")
 
     # STEP 3: Clean Up Insulation
     transaction = db.Transaction(doc)
@@ -115,13 +113,13 @@ def cleanup_insulation(pair):
     host_workset_parameter.Set(host_workset_id)
 
 
-def find_rogue_elements(document, insulation_elements):
+def find_rogue_elements(doc, elems):
     """Find all unhosted or rogue insulation elements."""
     unhosted_elements = []
     rogue_elements = []
-    for element in insulation_elements:
+    for element in elems:
         host_id = element.HostElementId
-        host_element = document.GetElement(host_id)
+        host_element = doc.GetElement(host_id)
         if host_element is None:
             unhosted_elements.append(element)
         else:
@@ -130,10 +128,10 @@ def find_rogue_elements(document, insulation_elements):
     return rogue_elements, unhosted_elements
 
 
-def query_all_elements(document, category):
+def query_all_elements(doc, cat):
     """Return all elements of a category from a document."""
-    filter = db.ElementCategoryFilter(category)
-    collector = db.FilteredElementCollector(document)
+    filter = db.ElementCategoryFilter(cat)
+    collector = db.FilteredElementCollector(doc)
     elements = collector.WherePasses(filter)\
                         .WhereElementIsNotElementType()\
                         .ToElements()
@@ -141,39 +139,35 @@ def query_all_elements(document, category):
     return elements
 
 
-def print_summary(element_list, caption=""):
+def print_summary(elems, caption=""):
     """Print a summary of the given list."""
-    length = element_list.Count
+    length = elems.Count
     print("{caption} {length}".format(caption=caption, length=length))
 
 
-def print_all(element_list, caption=None, indent=0):
+def print_all(elems, caption=None, indent=0):
     """Print all elements from the given list."""
-    total = element_list.Count
+    total = elems.Count
     if caption:
         print(caption)
-    for index, element in enumerate(element_list):
+    for index, element in enumerate(elems):
         print("{indent}[{index}/{total}]: {element}".format(
             indent=indent*" ",
             index=index,
             total=total,
             element=element))
 
-def write_report(file_path, unhosted_pipe, rogue_pipe, unhosted_duct, rogue_duct):
+def write_report(path, upipe, rpipe, uduct, rduct):
     """Write report of unhosted and rogue insulation elements to a file."""
-    print("writing report at {}".format(file_path))
-    with open(file_path, mode="w") as file:
+    print("writing report at {}".format(path))
+    with open(path, mode="w") as file:
         file.write("Hello World!\n")
         # TODO: implement output time stamp
         # TODO: implement report creation
 
 
 if __name__ == "__main__":
-    start = time.clock()
     main()
-    runtime = time.clock() - start
-    print("Runtime = {0} seconds".format(runtime))
     # revit python shell console management
-    raw_input("Hit any key to close.")
     __window__.Hide()
     __window__.Close()
