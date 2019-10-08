20d972adab618713c55cdebf8cff281f76c85183
Lukas Reimer <lukas.reimer@burohappold.com>
2019-10-08 08:55:09 +0200

    Create CorrectVentTags.py

---
+++ RevitPythonScripts/CorrectVentTags.py
@@ -0,0 +1,8 @@
+"""Correct ventilation pipe riser tags.
+
+This script checks through all pipe riser tags in the current view
+and corrects them to ventilation style tags if the tagged pipes system
+is a ventilation system.
+"""
+
+pass  # TODO: implement
