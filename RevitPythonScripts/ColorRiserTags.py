ed64882a097af357af4d34ef641d4a9b099a1da9
Lukas Reimer <Lukas.Reimer@burohappold.com>
2019-10-08 09:10:19 +0200

    Changed tabs to spaces and added riser tagging RPS addin manifest

--- RevitPythonScripts/ColorRiserTags.py
+++ RevitPythonScripts/ColorRiserTags.py
@@ -15,46 +15,46 @@ __version = "0.1b"
 
 
 def main():
-	"""Main script."""
+    """Main script."""
 
-	print("Running {fname} version {ver}...".format(fname=__name, ver=__version))
+    print("Running {fname} version {ver}...".format(fname=__name, ver=__version))
 
-	# STEP 0: Setup
-	doc = __revit__.ActiveUIDocument.Document
-	view = doc.ActiveView
+    # STEP 0: Setup
+    doc = __revit__.ActiveUIDocument.Document
+    view = doc.ActiveView
 
-	# STEP 1: Get all the tags in the view
-	tags = db.FilteredElementCollector(doc, view.Id)\
-			 .OfCategory(db.BuiltInCategory.OST_PipeTags)\
-			 .WhereElementIsNotElementType()\
-			 .ToElements()
-	#print(tags)
+    # STEP 1: Get all the tags in the view
+    tags = db.FilteredElementCollector(doc, view.Id)\
+             .OfCategory(db.BuiltInCategory.OST_PipeTags)\
+             .WhereElementIsNotElementType()\
+             .ToElements()
+    #print(tags)
 
-	# STEP 2: Override tag color based on host system type color
-	transaction = db.Transaction(doc)
-	transaction.Start("{name} - v{ver}".format(name=__name, ver=__version))
-	try:
-		for tag in tags:
-			host = tag.GetTaggedLocalElement()
-			system = host.MEPSystem
-			if system:
-				system_type = doc.GetElement(system.GetTypeId())
-				color = system_type.LineColor
-				override = db.OverrideGraphicSettings()
-				view.SetElementOverrides(tag.Id, override)  # reset to default
-				override.SetProjectionLineColor(color)
-				view.SetElementOverrides(tag.Id, override)
-	except Exception as ex:
-		print(ex)
-		transaction.RollBack()
-		return ui.Result.Failed
-	else:
-		transaction.Commit()
-		print("Done.")
-		return ui.Result.Succeeded
+    # STEP 2: Override tag color based on host system type color
+    transaction = db.Transaction(doc)
+    transaction.Start("{name} - v{ver}".format(name=__name, ver=__version))
+    try:
+        for tag in tags:
+            host = tag.GetTaggedLocalElement()
+            system = host.MEPSystem
+            if system:
+                system_type = doc.GetElement(system.GetTypeId())
+                color = system_type.LineColor
+                override = db.OverrideGraphicSettings()
+                view.SetElementOverrides(tag.Id, override)  # reset to default
+                override.SetProjectionLineColor(color)
+                view.SetElementOverrides(tag.Id, override)
+    except Exception as ex:
+        print(ex)
+        transaction.RollBack()
+        return ui.Result.Failed
+    else:
+        transaction.Commit()
+        print("Done.")
+        return ui.Result.Succeeded
 
 
 if __name__ == "__main__":
-	__window__.Hide()
-	__result = main()
-	__window__.Close()
+    __window__.Hide()
+    __result = main()
+    __window__.Close()
